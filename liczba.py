82c17d208c1603e862f10745b7693d950766faec
JedrzejPeziak <jedrzejpeziak@gmail.com>
2019-12-07 09:09:17 +0100

    zadanie rozgrzewka

---
+++ liczba.py
@@ -0,0 +1,10 @@
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    def wyzeruj(self):
+        self.value = 0
+
+    def ustaw(self, wartosc):
+        self.value = wartosc
+
---
+++ zeroabsolutne.py
@@ -0,0 +1,9 @@
+from liczba import Number
+
+
+class AbsolutZero(Number):
+    def __str__(self):
+        print("Jestem Zerem!")
+
+    def reklama(self):
+        print("UCZ SIE PAN")
